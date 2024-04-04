0e9e8db7c06006529631417754cab9be888a7f00
nakul71 <nakul.71@icloud.com>
2024-04-05 00:48:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+gsap.from("#nav h4",{
+    y:-50,
+    x:-50,
+    delay:0.2,
+    duration:0.5,
+    opacity:0,
+    stagger:0.1,
+})
+
+gsap.from("#head h1", {
+    
+    delay:0.2,
+    duration:0.5,
+    opacity:0,
+    stagger:0.1,
+    scale:1000,
+})
